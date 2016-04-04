8efd983d34ff7c4a1d8da97a4dfa9516af1b8b16
LIUHUANUCAS <liu2012huan@126.com>
2016-04-04 11:25:49 +0800

    begin learn redis

---
+++ redis.c
@@ -0,0 +1,12 @@
+/*********************************************************
+* File Name: redis.c
+* Author: LIUHUAN
+* Email: liu2012huan@126.com | liuhuan@ict.ac.cn
+* Created Time: 2016年04月04日 星期一 11时25分07秒
+*********************************************************/
+
+#include<stdio.h>
+int main(){
+	printf("begin learning redis...\n");
+	return 0;
+}
